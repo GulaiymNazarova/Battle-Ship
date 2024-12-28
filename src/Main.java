805696b4297341aa1655fbb71ee06444adce218d
Nazarova Gulaiym <gulaiym.nazarova@alatoo.edu.kg>
2024-12-28 23:03:36 +0600

    Greet and ask for users name

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Battle Ship! \nPlease enter your name:");
+        int userName = sc.nextInt();
+    }
+}
