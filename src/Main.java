98502d38c3370dfa936ae690f52f8f6dd26308bd
Nazarova Gulaiym <gulaiym.nazarova@alatoo.edu.kg>
2025-01-03 19:15:08 +0600

    Create field

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,38 @@ public class Main
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Battle Ship! \nPlease enter your name:");
-        int userName = sc.nextInt();
+        String userName = sc.nextLine();
+        // The field
+        String[][] field = new String[8][8];
+        field[0][0] = " ";
+        field[0][1] = "A";
+        field[0][2] = "B";
+        field[0][3] = "C";
+        field[0][4] = "D";
+        field[0][5] = "E";
+        field[0][6] = "F";
+        field[0][7] = "G";
+
+        for(int j = 1; j < 8; j++)
+        {
+            field[j][0] = String.valueOf(j);
+        }
+        for(int j = 1; j < 8; j++)
+        {
+            for(int i = 1; i < 8; i++)
+            {
+                field[j][i] = "~";
+            }
+        }
+
+        for(int j = 0; j < 8; j++)
+        {
+            for(int i = 0; i < 8; i++)
+            {
+                System.out.print(field[j][i] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
